20a555bb3c38d38738e667cc6f01aacbe7c1b184
schmaigul <miksuhok@gmail.com>
2022-10-05 11:39:43 +0300

    Regular connect four works now

--- ConnectFour.py
+++ ConnectFour.py
@@ -1,6 +1,6 @@
 import pygame
 import numpy as np
-from src.connectfour import ConnectFour
+from utils.board_logic import *
 
 #Choose the number of columns and rows for the game
 OFF_WHITE = (248, 240, 227)
@@ -10,29 +10,28 @@ RED = (212, 57, 91)
 BLUE = (67, 57, 212)
 
 
-def play_connect_four(board):
+def play_connect_four(board, turn):
     """Game loop for two player connect four. The loop exits when the game has ended"""
     
     #Setting up game variables
-    turn = board.get_turn()
     game_not_over = True
     turn_texts = ["Red's turn", "Blue's turn"]
     turn_colors = [RED, BLUE]
-    rows, columns = board.get_dimensions()
+    rows, columns = board.shape
     
     #Setting up the game window
     window = pygame.display.set_mode(size = (700, 700))
     pygame.display.set_caption('Connect Four')
     font = pygame.font.Font('freesansbold.ttf', 32)
-    board.draw(window)
+    draw_board(board, window)
+
     text = font.render(turn_texts[turn-1], True, turn_colors[turn-1])
     window.blit(text, dest = (250, 40))
+
     pygame.display.update()
 
     #Game loop, players are 1 (red) and 2 (blue)
     while(game_not_over):
-
-        turn = board.get_turn()
 
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -43,17 +42,18 @@ def play_connect_four(board):
                 playerPosition = int(event.pos[0]/SQUARE_SIZE)
                 print(playerPosition)
 
-                if (board.check_valid(playerPosition)):
-                    board.place_disc(playerPosition) 
+                if (check_valid(board, playerPosition)):
+                    board, row = place_disc(board, rows, playerPosition, turn)
+
                 else:
                     print('Not a valid position, choose a new one')
                     break
                 #Swithes turn of the player between 1 and 2
-                if board.check_draw():
+                if check_draw(board):
                     game_not_over = False
                     text = font.render('Draw!', True, (255,255,255))
 
-                if board.check_win():
+                if check_win(board, turn):
                     game_not_over = False
 
                     if (turn == 1):
@@ -62,12 +62,12 @@ def play_connect_four(board):
                         text =  font.render('Blue wins!', True, turn_colors[turn-1])
 
                 #Game is draw if there are no 0's in the array
-                if (not board.check_win() and not board.check_draw()): 
+                if (not check_win(board, turn) and not check_draw(board)): 
                     #If not end state, change turn
-                    turn = board.change_turn()
+                    turn = change_turn(turn)
                     text = font.render(turn_texts[turn-1], True, turn_colors[turn-1])
 
-                board.draw(window)
+                draw_board(board, window)
                 window.blit(text, dest = (250, 40))
                 pygame.display.update()
 
@@ -78,6 +78,6 @@ def play_connect_four(board):
 if __name__ == '__main__':
     pygame.init()
     while (True):
-        board = ConnectFour((np.zeros((6,7), int)), 1)
-        play_connect_four(board)
+        board = np.zeros((6,7), int)
+        play_connect_four(board, 1)
        
